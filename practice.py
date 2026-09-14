381042e8302be51a7d069cc7536203c157b2e227
danielchen-jpg <daniel.chen@pulaskiacademy.org>
2026-09-13 22:32:14 -0500

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,10 @@
+county = "Pulaski County"
+agricultural_exports = 1000000
+tariff_rate = 0.10
+
+potential_effect = agricultural_exports * tariff_rate
+
+print("County:", county)
+print("Agricultural exports:", agricultural_exports)
+print("Tariff rate:", tariff_rate)
+print("Potential modeled effect:", potential_effect)
